b3c013f4c9169e875d08c4a9b7dfefd4d85e6e8b
kevinc655 <cheng9062@mydusd.org>
2025-03-24 13:46:09 -0700

    b

--- Bird.java
+++
@@ -1,7 +0,0 @@
-public class Bird extends Animal {
-    public Bird(String food, boolean nocturnal, double aveLifeSpan){
-        super(food, nocturnal, aveLifeSpan);
-        System.out.println("The new arrival is a bird!");
-    }
-    public Bird(){}
-}
--- Lion.java
+++
@@ -1,5 +0,0 @@
-public class Lion extends Feline {
-    public void huntInPack(){
-        System.out.println("The lion hunts in a pack");
-    }
-}
